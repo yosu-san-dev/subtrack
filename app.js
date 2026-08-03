8ab8b7342c817e8fe244092220d2e45f8699af7f
Yusuf fatthi <yusuf4yt2001@gmail.com>
2026-08-03 19:02:04 +0300

    Update backend for frontend integration, CORS, and fixes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import { PORT } from './config/env.js';
 import { initDatabase } from './database/sqlite.js';
@@ -14,6 +15,7 @@ import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
